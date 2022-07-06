6ff56f5033aa8da2f99c326ef22a2ae826384d3a
pythondocs <107457541+pythondocs@users.noreply.github.com>
2022-07-06 12:36:52 +0530

    complete

--- Homeshop18/homeshop18/homeshop18/urls.py
+++ Homeshop18/homeshop18/homeshop18/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
-    path('client', include('client.urls')),
+    path('', include('app.urls'))
 ]
